14f3ceef3d04852cd858b8d578384ef0c7004e45
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 23:07:20 +0700

    add object DestinasiEntry on HalamanEntry.kt

--- app/src/main/java/com/example/act9/ui/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/act9/ui/Halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.act9.ui.Halaman
 
+import com.example.act9.R
+import com.example.act9.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
