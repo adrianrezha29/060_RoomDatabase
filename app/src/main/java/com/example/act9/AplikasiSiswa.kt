1d4dfc7d6023e1fc5917e21e68219683cc5f7bbf
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 22:54:36 +0700

    add ContainerApp on AplikasiSiswa.kt

--- app/src/main/java/com/example/act9/AplikasiSiswa.kt
+++ app/src/main/java/com/example/act9/AplikasiSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.act9
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.act9.Repositori.ContainerApp
+import com.example.act9.Repositori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container : ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
