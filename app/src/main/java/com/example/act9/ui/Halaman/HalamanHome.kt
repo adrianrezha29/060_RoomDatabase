b1dad425d846831f991d24887fa81d2b518d8274
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 23:36:17 +0700

    add ListSiswa on HalamanHome.kt

--- app/src/main/java/com/example/act9/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/act9/ui/Halaman/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.act9.ui.Halaman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -91,6 +93,23 @@ fun BodyHome(
                 itemSiswa = itemSiswa,
                 modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
             )
+        }
+    }
+}
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier)
+{
+    LazyColumn(
+        modifier = Modifier
+    ){
+        items(
+            items = itemSiswa, key = {it.id}){
+                person ->
+            DataSiswa(siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small)))
         }
     }
 }
