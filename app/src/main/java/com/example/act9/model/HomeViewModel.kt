037f8fb3689e3b98be3437b36147f6cbc5d1424f
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 22:46:54 +0700

    add private const val on HomeViewModel.kt

--- app/src/main/java/com/example/act9/model/HomeViewModel.kt
+++ app/src/main/java/com/example/act9/model/HomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.act9.model
 
+import androidx.lifecycle.ViewModel
+import com.example.act9.Repositori.RepositoriSiswa
+
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
