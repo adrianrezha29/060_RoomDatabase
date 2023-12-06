b3bcdbe22a994ac6af588a52da1609de55e3fe23
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 23:19:30 +0700

    add object DestinasiHome on HalamanHome.kt

--- app/src/main/java/com/example/act9/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/act9/ui/Halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.act9.ui.Halaman
 
+import com.example.act9.R
+import com.example.act9.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
