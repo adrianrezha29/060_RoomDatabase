a3be9aaa80539b13f475488d71ae9c5664d106e8
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 23:10:37 +0700

    add EntrySiswaScreen on HalamanEntry.kt

--- app/src/main/java/com/example/act9/ui/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/act9/ui/Halaman/HalamanEntry.kt
@@ -1,9 +1,25 @@
 package com.example.act9.ui.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.act9.R
+import com.example.act9.model.EntryViewModel
+import com.example.act9.model.PenyediaViewModel
 import com.example.act9.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
