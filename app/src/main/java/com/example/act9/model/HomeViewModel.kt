28a311b7a9bbb55785431b6cf02ed6a9f799e750
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-06 22:50:02 +0700

    add data class HomeUiState on HomeViewModel.kt

--- app/src/main/java/com/example/act9/model/HomeViewModel.kt
+++ app/src/main/java/com/example/act9/model/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.act9.model
 
 import androidx.lifecycle.ViewModel
+import com.example.act9.Data.Siswa
 import com.example.act9.Repositori.RepositoriSiswa
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
